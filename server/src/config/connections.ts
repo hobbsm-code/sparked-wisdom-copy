2df9b1609f49e09908919e10123033645cd2c923
Mark Hobbs <hobbsm121@gmail.com>
2024-12-09 16:56:02 -0500

    config for render

--- server/src/config/connections.ts
+++ server/src/config/connections.ts
@@ -1,16 +1,22 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
+let sequelize: Sequelize;
+
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
-  process.env.DB_USER as string,
-  process.env.DB_PASSWORD as string,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-  }
-);
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
 
+  sequelize = new Sequelize(
+    process.env.DB_NAME as string,
+    process.env.DB_USER as string,
+    process.env.DB_PASSWORD as string,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
+}
 export default sequelize;
